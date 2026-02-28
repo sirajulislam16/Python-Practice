be1d51b691e48375002b5e04f00f62cad8adb97e
sirajulislam16 <sirajulislam160808@gmail.com>
2026-03-01 00:11:06 +0600

    gradeCalculator

---
+++ decision making/gradeCalculator.py
@@ -0,0 +1,11 @@
+exam_num= float(input("ENter your number: "))
+if exam_num>=80:
+    print("A")
+if exam_num>=70 and exam_num<=79:
+    print("B")
+if exam_num>=60 and exam_num<=69:
+    print("C")
+if exam_num>=50 and exam_num<=59:
+    print("D")
+if exam_num<50:
+    print("F")
