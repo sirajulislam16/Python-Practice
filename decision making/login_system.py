4d4acd4b00ae663525641eed654b25a432fb2af1
sirajulislam16 <sirajulislam160808@gmail.com>
2026-03-01 00:25:04 +0600

    loging system by decision making statement

---
+++ decision making/login_system.py
@@ -0,0 +1,13 @@
+correct_username= "admin"
+correct_pssword = "01642"
+user_username = input("Enter your username: ")
+user_pass = input("Enter your password: ")
+
+if user_username==correct_username and user_pass==correct_pssword:
+    print("Loging Successfully")
+elif user_username!=correct_username and user_pass== correct_pssword:
+    print("Username is wrong")
+elif user_username!=correct_username and user_pass!=correct_pssword:
+    print("Username and User Password are wrong")
+elif user_username==correct_username and user_pass!=correct_pssword:
+    print("Password is invalid")
