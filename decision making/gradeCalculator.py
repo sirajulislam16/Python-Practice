f43e91796f17a3ef2c607efc6d03c4b4ac10538f
sirajulislam16 <sirajulislam160808@gmail.com>
2026-03-01 09:21:45 +0600

    correct elif instead of if in every line

--- decision making/gradeCalculator.py
+++ decision making/gradeCalculator.py
@@ -1,11 +1,13 @@
 exam_num= float(input("ENter your number: "))
 if exam_num>=80:
     print("A")
-if exam_num>=70 and exam_num<=79:
+elif exam_num>=70 and exam_num<=79:
     print("B")
-if exam_num>=60 and exam_num<=69:
+elif exam_num>=60 and exam_num<=69:
     print("C")
-if exam_num>=50 and exam_num<=59:
-    print("D")
-if exam_num<50:
+elif exam_num>=50 and exam_num<=59:
+  print("D")
+elif exam_num<50:
     print("F")
+elif exam_num<0 or exam_num>100:
+    print("Ivalid Marks")
--- decision making/login_system.py
+++ decision making/login_system.py
@@ -5,9 +5,10 @@ user_pass = input("Enter your password: ")
 
 if user_username==correct_username and user_pass==correct_pssword:
     print("Loging Successfully")
+elif user_username==correct_username and user_pass!=correct_pssword:
+    print("Password is invalid")
 elif user_username!=correct_username and user_pass== correct_pssword:
     print("Username is wrong")
 elif user_username!=correct_username and user_pass!=correct_pssword:
     print("Username and User Password are wrong")
-elif user_username==correct_username and user_pass!=correct_pssword:
-    print("Password is invalid")
+
