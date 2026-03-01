a01a06dc67871db78076a8dca4e849f6515c2332
sirajulislam16 <sirajulislam160808@gmail.com>
2026-03-01 14:46:42 +0600

    reverse a number

---
+++ for_while_loops.py/reverse_a_number.py
@@ -0,0 +1,12 @@
+num = input("Enter your number: ")
+# rev = 0
+# while num > 0:
+#     d = num%10
+#     rev = rev*10+d
+#     num //=10
+# print("Your reversed number: ",rev)
+
+
+# second method
+reverse_num= num[::-1]
+print("Your reversed number: ",reverse_num)
---
+++ for_while_loops.py/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+v = 0
+# while num > 0:
+#     d = num%10
+#     rev = rev*10+d
+#     num //=10
